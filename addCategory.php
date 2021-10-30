9ac74a803f413b54e2d755490a5dc61253ff81da
EmilyF99 <emilyfletcher099@gmail.com>
2021-10-30 22:14:08 +0100

    Added Layout to addCategory

--- addCategory.php
+++ addCategory.php
@@ -1,2 +1,33 @@
+
+<!-- Head PHP contains the html head as well as the webpages header-->
 <?php
+require 'head.php';
+?>
+
+<!--contains the main navigation of the page and the page banner -->
+<!--This is seperate from the head.php file to make the navigation easier to improve in the future -->
+<?php
+require 'nav.php';
+?>
+
+<!-- Delete the <nav> element if the sidebar is not required -->
+<nav>
+	<ul>
+		<li><a href="#">Sidebar</a></li>
+		<li><a href="#">This can</a></li>
+		<li><a href="#">Be removed</a></li>
+		<li><a href="#">When not needed</a></li>
+	</ul>
+</nav>
+
+<article>
+<form>
+	<p>Add a Category:</p>
+
+		<label>Category Title</label> <input type="text" />
+		<input type="submit" name="submit" value="Submit" />
+</form>
+<!-- contains the page footer and the closing html -->
+<?php
+require 'foot.php';
 ?>
