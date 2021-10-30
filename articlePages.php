3395014c102088a6d9dd4bc9ca4e1e95bc4723e8
EmilyF99 <emilyfletcher099@gmail.com>
2021-10-30 22:32:38 +0100

    added layout to deleteArticle + xtra not in brief

--- articlePages.php
+++ articlePages.php
@@ -1,2 +1,34 @@
+
+<!-- Head PHP contains the html head as well as the webpages header-->
 <?php
+require 'head.php';
+?>
+
+<!--contains the main navigation of the page and the page banner -->
+<!--This is seperate from the head.php file to make the navigation easier to improve in the future -->
+<?php
+require 'nav.php';
+?>
+
+<!-- Delete the <nav> element if the sidebar is not required -->
+<nav>
+	<ul>
+		<li><a href="#">Sidebar</a></li>
+		<li><a href="#">This can</a></li>
+		<li><a href="#">Be removed</a></li>
+		<li><a href="#">When not needed</a></li>
+	</ul>
+</nav>
+
+<article>
+<form>
+    <p>Forms are styled like so:</p>
+
+    <label>Field 1</label> <input type="text" />
+    <label>Field 2</label> <input type="text" />
+    <label>Textarea</label> <textarea></textarea>
+</form>
+<!-- contains the page footer and the closing html -->
+<?php
+require 'foot.php';
 ?>
