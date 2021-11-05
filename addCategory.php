5053d62bda287b5e7a6dcaade75b53b7b03abe2f
EmilyF99 <emilyfletcher099@gmail.com>
2021-11-05 16:32:22 +0000

    Can now add category IDs, does not update nav bar

--- addCategory.php
+++ addCategory.php
@@ -1,27 +1,39 @@
 
 <!-- Head PHP contains the html head as well as the webpages header-->
-<?php
-require 'head.php';
-?>
-
 <!--contains the main navigation of the page and the page banner -->
 <!--This is seperate from the head.php file to make the navigation easier to improve in the future -->
 <?php
+require 'head.php';
 require 'nav.php';
-?>
-
-<!-- Code for Side Bar Navigation (remove when not needed) -->
-<?php 
 require 'sideNavBar.php';
+require 'databaseJoin.php';
+?>
+<?php
+if(isset($_POST['submit'])) {
+    $stmt = $pdo->prepare('INSERT INTO category(name) 
+                                       VALUES(:name)
+    ');
+
+    $values = [
+    'name' => $_POST['name']
+    ];
+
+    $stmt->execute($values);
+    echo '<p> Your Category Has Been Added </p>';
+}
+
+else {
+    echo '<article>
+	<form action="addCategory.php" method="POST">
+		<p>Add a Category:</p>
+	
+			<label>Category Title</label> <input name = "name" type="text" required />
+			<input type="submit" name="submit" value="submit" />
+	</form>';  
+}
 ?>
 
-<article>
-<form>
-	<p>Add a Category:</p>
 
-		<label>Category Title</label> <input type="text" required />
-		<input type="submit" name="submit" value="Submit" />
-</form>
 <!-- contains the page footer and the closing html -->
 <?php
 require 'foot.php';
