23b435c15854422c4ff8075c4255fbda1c7064d0
EmilyF99 <emilyfletcher099@gmail.com>
2021-11-07 22:37:54 +0000

    addAdmin adds a new user

--- addAdmin.php
+++ addAdmin.php
@@ -24,45 +24,19 @@ if(isset($_POST['submit'])) {
     echo '<p> Your Request Has Been Submitted </p>';
 }
 
-<article>
-<form>
-	<p>Create a New Admin:</p>
-
-    <label>User Name</label> <input type="text" required/>
-    <label>Email address</label> <input type="text" required />
-    <label>Password</label> <input type = "text" required>
-	<input type="submit" name="Submit" value="Submit" />
-</form>
-<!-- contains the page footer and the closing html -->
-<?php
-require 'foot.php';
-?>
-
-<?php
-if(isset($_POST['submit'])) {
-    $stmt = $pdo->prepare('INSERT INTO users(name, email, password) 
-                                       VALUES(:name,:email,:password)
-    ');
-
-    $values = [
-    'name' => $_POST['name'],
-    'email'=> $_POST['email'],
-    'password' => $_POST['password']
-    ];
-
-    $stmt->execute($values);
-    echo '<p> Your Request Has Been Submitted </p>';
-}
-
 else {
     echo '<article>
-    <form action="register.php" method="POST">
-        <p>Register for an Account:</p>
+    <form action="addAdmin.php" method="POST">
+        <p>Create a New Admin:</p>
     
         <label>User Name</label> <input name = "name" type="text" required/>
         <label>Email address</label> <input name = "email" type="text" required />
         <label>Password</label> <input name = "password" type = "text" required>
         <input type="submit" name="submit" value="submit" />
-    </form>';  
+    </form>';
 }
+?>
+<!-- contains the page footer and the closing html -->
+<?php
+require 'foot.php';
 ?>
