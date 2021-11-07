4a5da9b70b4cdfd6563c7f2952cda96387acd558
EmilyF99 <emilyfletcher099@gmail.com>
2021-11-07 21:45:18 +0000

    Categories are pulled into the nav

--- nav.php
+++ nav.php
@@ -1,11 +1,20 @@
+<?php
+	require 'databaseJoin.php';
+?>
 <nav>
 			<ul>
 				<li><a href="index.php">Latest Articles</a></li>
 				<li><a href="#">Select Category</a>
 					<ul>
+
+						<?php 
+							$results = $pdo->query('SELECT * FROM category');
+
+							foreach ($results as $row) {
+								echo '<li><a class="articleLink" href="#">' . $row['name'] . '</a></li>';
+							}
+						?>
 						<li><a class="articleLink" href="#">Category 1</a></li>
-						<li><a class="articleLink" href="#">Category 2</a></li>
-						<li><a class="articleLink" href="#">Category 3</a></li>
 					</ul>
 					<li><a class="articleLink" href="register.php">Register </a></li>
 					<li><a class="articleLink" href="login.php">Log in </a></li>
