cdda6e6aad00737cac8188944cc06267bf8286e2
EmilyF99 <emilyfletcher099@gmail.com>
2021-11-11 03:17:30 +0000

    edits to match brief requirement

--- categoryPages.php
+++ categoryPages.php
@@ -16,7 +16,7 @@
         $selectCat -> execute($value);
 
         foreach($selectCat->fetchAll() as $row) {
-            echo '<a class="articleLink" href="articlePages.php?articleId=' . $row['articleId'] . '">' . '<p>'.$row['title'] . '</p></a>';
+            echo '<a class="articleLink" href="articlePages.php?articleId=' . $row['articleId'] . '">' . '<p>'.$row['title'] . '</a>' .' Published: '.$row['publishDate'] . '</p>';
         }
        }
      
