45d5fddaa7f41de439a3bb59be7508035ea5a49a
EmilyF99 <emilyfletcher099@gmail.com>
2021-11-21 17:13:49 +0000

    final comments added

--- register.php
+++ register.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
+/*The requires are used to generate the templates as well as connect to the database
+These are not stored in the public directory as they user should not be able to directly access these*/
 $title = 'Northampton News - Register';
 require '../head.php';
 require '../nav.php';
 require '../databaseJoin.php';
-?>
 
-<?php
+/*If submit is clicked then the asccount details added by the user are added to the users table  */
 if(isset($_POST['submit'])) {
     $stmt = $pdo->prepare('INSERT INTO users(name, email, password) 
                                        VALUES(:name,:email,:password)
     ');
 
+    //the password entered is hashed and salted using the code belwo
     $hashing = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
     $values = [
     'name' => $_POST['name'],
     'email'=> $_POST['email'],
@@ -20,9 +23,11 @@ if(isset($_POST['submit'])) {
     ];
 
     $stmt->execute($values);
+    //when executed the user will be given a prompt telling them that they're request has been received
     echo '<p> Your Request Has Been Submitted </p>';
 }
 
+//the else statement is used to generate the empty form when the user hasn't pressed submit
 else {
     echo '<article>
     <form action="register.php" method="POST">
@@ -34,9 +39,6 @@ else {
         <input type="submit" name="submit" value="submit" />
     </form>';  
 }
-?>
 
-<!-- contains the page footer and the closing html -->
-<?php
 require '../foot.php';
 ?>
--- userComments.php
+++ userComments.php
@@ -1,20 +1,27 @@
 <?php
 session_start();
+/*The requires are used to generate the templates as well as connect to the database
+These are not stored in the public directory as they user should not be able to directly access these*/
 $title = 'Northampton News - Comments';
 require '../head.php';
 require '../nav.php';
 require '../databaseJoin.php';
 
-
+    /*the prepare statement selects all from the comments section where a matching user name has been found 
+    from the identifier in the URL, See articlePages to see how this  unique comment link is generateed  */
     $displayComm = $pdo->prepare('SELECT * FROM comment WHERE username= :username');
+
     $value = [
         'username' => $_GET['username'],
     ];
 
     $displayComm -> execute($value);
 
+    //the username is displayed as a page title
     echo '<h2>' . $_GET['username'] . ' </h2>';
 
+    /*all the information about a comment is retrieved using this foreach loop, 
+    each record will display the comment and the article ID of the article it came from */
     foreach($displayComm->fetchAll() as $row) {
 
         echo  '<p>Comment Posted: "' .$row['commentContent'] .'" </p>';
